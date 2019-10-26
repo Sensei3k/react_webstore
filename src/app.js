80616eaf6cef1b4d39426abc05c8b0b29fc9cfc9
Detola Oyawoye <oyawoye.ade@gmail.com>
2019-10-26 16:36:57 +0100

    created Navbar component with scss file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Navbar from '../src/components/Navbar'
+
 import '../sass/style.scss'
 
 class App extends React.Component {
   render() {
-    return (<div>Hello Sensei!</div>)
+    return (
+      <div className="main-container">
+        <Navbar />
+      </div>
+    )
   } 
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../../sass/navbar.scss'
+
+class Navbar extends React.Component {
+  render() {
+    return(
+      <section className="Navbar">
+        <p><img src="/assets/telephone.png" /> 1337 1337 1337</p>
+        <p><img src="/assets/flag.png" /> Try another Castle</p>
+      </section>
+    )
+  }
+}
+
+export default Navbar
