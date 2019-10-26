2be255a3be6821d0681886f124d70f6728e5598e
Detola Oyawoye <oyawoye.ade@gmail.com>
2019-10-26 21:07:04 +0100

    completed styling for ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,22 +2,21 @@ import React from 'react'
 
 import '../../sass/productCard.scss'
 
-const ProductCard = ({ title, description, image, price }) => {
-  return(
-    <div className="projects-info-wrapper">
-      <div>
-        <img src={image} />
+const ProductCard = ({ title, description, image, price, addToBasket }) => {
+  return (
+    <div className='projects-info-wrapper'>
+      <div className='project-block'>
+        <img className='image' src={image} alt={title} />
         <div>
           <h4>{title}</h4>
-          <span>{description}</span>
-          <span>
-            <img src="/assets/coins.png" alt="" />
+          <span className='description'>{description}</span>
+          <span className='price'>
+            <img src='/assets/coins.png' alt='' />
             {price}
           </span>
-          <button>
-            <span>
-              Add to Basket
-            </span>
+          <button className="button"
+            onClick={() => addToBasket()}>
+            <span>Add to Basket</span>
           </button>
         </div>
       </div>
